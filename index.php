e5bafbd1341cb891402b6b4bf8d0ce6bc96e4d3b
Lazare Fortune <lazarefortune@gmail.com>
2022-06-14 23:10:45 +0200

    Add section contact me

--- index.php
+++ index.php
@@ -691,18 +691,77 @@
             </section>
 
             <!--==================== PROJECT IN MIND ====================-->
-            <section class="project section">
+            <!-- <section class="project section">
 
-            </section>
+            </section> -->
 
             <!--==================== TESTIMONIAL ====================-->
-            <section class="testimonial section">
+            <!-- <section class="testimonial section">
                 
-            </section>
+            </section> -->
 
             <!--==================== CONTACT ME ====================-->
             <section class="contact section" id="contact">
+                <h2 class="section__title">Contact Me</h2>
+                <span class="section__subtitle">Get in touch</span>
 
+                <div class="contact__container container grid">
+                    <div>
+                        <div class="contact__information">
+                            <i class="uil uil-phone contact__icon"></i>
+                            
+                            <div>
+                                <h3 class="contact__title">Call Me</h3>
+                                <span class="contact__subtitle">+33 6 61 43 47 99</span>
+                            </div>
+                        </div>
+
+                        <div class="contact__information">
+                            <i class="uil uil-envelope contact__icon"></i>
+                            
+                            <div>
+                                <h3 class="contact__title">Email</h3>
+                                <span class="contact__subtitle">lazarefortune@gmail.com</span>
+                            </div>
+                        </div>
+
+                        <div class="contact__information">
+                            <i class="uil uil-map-marker contact__icon"></i>
+                            
+                            <div>
+                                <h3 class="contact__title">Location</h3>
+                                <span class="contact__subtitle">11 Av. Auguste Rodin 94350 Villiers-sur-marne</span>
+                            </div>
+                        </div>
+
+                        <form action="" method="post" class="contact__form grid">
+                            <div class="contact__inputs grid">
+                                <div class="contact__content">
+                                    <label for="" class="contact__label">Name</label>
+                                    <input type="text" name="" id="" class="contact__input">
+                                </div>
+                                <div class="contact__content">
+                                    <label for="" class="contact__label">Email</label>
+                                    <input type="email" name="" id="" class="contact__input">
+                                </div>
+                            </div>
+                            <div class="contact__content">
+                                <label for="" class="contact__label">Project</label>
+                                <input type="text" name="" id="" class="contact__input">
+                            </div>
+                            <div class="contact__content">
+                                <label for="" class="contact__label">Message</label>
+                                <textarea name="" id="" cols="0" rows="7" class="contact__input"></textarea>
+                            </div>
+                            <div>
+                                <a href="" class="button button--flex">
+                                    Send Message
+                                    <i class="uil uil-message button__icon"> </i>
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </section>
         </main>
 
